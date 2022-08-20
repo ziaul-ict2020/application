0158c05d66e497c4983b7c3bb2f2de83249e6ed7
Ziaul Haque <ziaul.uoda86@gmail.com>
2022-08-20 23:42:39 +0600

    Change something

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('This is Ziaul');
+  res.send('This is Ziaul. My first project');
 });
 
 app.listen(PORT, HOST);
